5bf3d4f635dbc07244b2fbe6f7df399000a71856
Thomas Stromberg <t+github@chainguard.dev>
2022-10-17 08:43:57 -0400

    Rename continuous to rapid

--- cmd/osqtool/main.go
+++ cmd/osqtool/main.go
@@ -34,7 +34,7 @@ func main() {
 	outputFlag := flag.String("output", "", "Location of output")
 	minIntervalFlag := flag.Duration("max-interval", 15*time.Second, "Queries can't be scheduled more often than this")
 	defaultIntervalFlag := flag.Duration("default-interval", 1*time.Hour, "Interval to use for queries which do not specify one")
-	tagIntervalsFlag := flag.String("tag-intervals", "transient=5m,postmortem=6h,continuous=15s,often=x/4,seldom=2x", "modifiers to the default-interval based on query tags")
+	tagIntervalsFlag := flag.String("tag-intervals", "transient=5m,postmortem=6h,rapid=15s,often=x/4,seldom=2x", "modifiers to the default-interval based on query tags")
 	maxIntervalFlag := flag.Duration("min-interval", 24*time.Hour, "Queries cant be scheduled less often than this")
 	excludeFlag := flag.String("exclude", "", "Comma-separated list of queries to exclude")
 	platformsFlag := flag.String("platforms", "", "Comma-separated list of platforms to include")
