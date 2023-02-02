3f7abda81a5894c8282c700377ff664218db975c
Thomas Stromberg <t+github@chainguard.dev>
2023-02-02 11:44:20 -0500

    Remove worker count update

--- cmd/osqtool/main.go
+++ cmd/osqtool/main.go
@@ -87,11 +87,7 @@ func main() {
 	}
 
 	if c.Workers < 1 {
-		// Leave threads for osqueryd as well by default
-		c.Workers = runtime.NumCPU() / 2
-		if c.Workers < 1 {
-			c.Workers = 1
-		}
+		c.Workers = runtime.NumCPU()
 		if *verifyFlag || action == "verify" {
 			klog.Infof("automatically setting verify worker count to %d", c.Workers)
 		}
